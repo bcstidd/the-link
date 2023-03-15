b232c3cd667e5d70c4471858770478342c8a7ef5
bcstidd <bc.stidd@gmail.com>
2023-03-15 10:57:16 -0700

    heroku fix

--- src/pages/StylePage/StylePage.jsx
+++ src/pages/StylePage/StylePage.jsx
@@ -1,36 +1,36 @@
 import { Link } from 'react-router-dom';
-import { artists } from '../../data';
+// import { artists } from '../../data';
 
-export default function StylePage() {
-  // Collect all the styles from the artists
-  const allStyles = artists.reduce((acc, artist) => {
-    artist.style.forEach((style) => {
-      if (!acc.includes(style)) {
-        acc.push(style);
-      }
-    })
-    return acc;
-  }, []);
+// export default function StylePage() {
+//   // Collect all the styles from the artists
+//   const allStyles = artists.reduce((acc, artist) => {
+//     artist.style.forEach((style) => {
+//       if (!acc.includes(style)) {
+//         acc.push(style);
+//       }
+//     })
+//     return acc;
+//   }, []);
 
-  return (
-    <div className='styleList'>
-      <h1>Styles</h1>
-      {allStyles.map((style) => (
-        <div key={style}>
-          <h2>{style}</h2>
-          <ul>
-            {artists
-              .filter(artist => artist.style.includes(style))
-              .map((artist) => (
-                <li key={artist.id}>
-                  <Link to={'/artists/selectedArtist'}>
-                    {artist.name}
-                  </Link> - {artist.location}
-                </li>
-              ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div className='styleList'>
+//       <h1>Styles</h1>
+//       {allStyles.map((style) => (
+//         <div key={style}>
+//           <h2>{style}</h2>
+//           <ul>
+//             {artists
+//               .filter(artist => artist.style.includes(style))
+//               .map((artist) => (
+//                 <li key={artist.id}>
+//                   <Link to={'/artists/selectedArtist'}>
+//                     {artist.name}
+//                   </Link> - {artist.location}
+//                 </li>
+//               ))}
+//           </ul>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
